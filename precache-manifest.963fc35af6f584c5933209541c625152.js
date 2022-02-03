10f223857967588d32397b571b0c118c1ef1974b
max suheckiy <suheckiymax@gmail.com>
2022-02-03 12:12:08 +0200

    Updates

---
+++ precache-manifest.963fc35af6f584c5933209541c625152.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "814804aec9e413bcf55e",
+    "url": "/be-millionaire/static/css/main.9b7fc123.chunk.css"
+  },
+  {
+    "revision": "814804aec9e413bcf55e",
+    "url": "/be-millionaire/static/js/main.814804ae.chunk.js"
+  },
+  {
+    "revision": "9f058885977fbe432418",
+    "url": "/be-millionaire/static/js/1.9f058885.chunk.js"
+  },
+  {
+    "revision": "3dce243ff680a574b25d",
+    "url": "/be-millionaire/static/js/runtime~main.3dce243f.js"
+  },
+  {
+    "revision": "9691cd3706128a8b555786a66f73e0c9",
+    "url": "/be-millionaire/static/media/hand.9691cd37.png"
+  },
+  {
+    "revision": "4d6bb050a8765236198b59fe8715697c",
+    "url": "/be-millionaire/index.html"
+  }
+];
